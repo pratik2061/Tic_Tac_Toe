e590c1e776a0ffccf8483dba0e18ebaef1d63460
pratik sharma <123287083+pratik2061@users.noreply.github.com>
2024-06-03 18:30:06 +0545

    ✨ Game completed

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset");
+let newGamebtn = document.querySelector(".new_game")
+let display_winner =document.querySelector(".display_winner")
+let winner = document.querySelector(".winner")
+
+display_winner.style.display = "none"
 
 let turn_O = true;
 
@@ -16,7 +21,6 @@ const winingPatterns =[
 
 boxes.forEach((box)=>{
  box.addEventListener("click",()=>{
-    console.log("clicked");
     if (turn_O == true) {
         box.innerText ="O";
         turn_O=false
@@ -33,11 +37,62 @@ boxes.forEach((box)=>{
 
 // }
 const checkwinner = ()=>{
- for (pattern of winingPatterns) {
-     
- }
-}
 
+//MY LOGIC
+
+//  for ( let pattern of winingPatterns) {
+//     if (boxes[pattern[0]].innerText==="O" && boxes[pattern[1]].innerText ==="O" && boxes[pattern[2]].innerText === "O") {
+//         alert("O is winner")
+//     }else if (boxes[pattern[0]].innerText==="X" && boxes[pattern[1]].innerText ==="X" && boxes[pattern[2]].innerText === "X") {
+//         alert("X is winner")
+//     }
+//  }
+
+for (pattern of winingPatterns) {
+    let position_0_val = boxes[pattern[0]].innerText
+    let position_1_val = boxes[pattern[1]].innerText
+    let position_2_val = boxes[pattern[2]].innerText
+
+    if (position_0_val != "" && position_1_val != "" && position_2_val != "") {
+        if (position_0_val == position_1_val && position_1_val == position_2_val) {
+         showWinner(position_0_val)
+        }
+    }
+}
+}
+const disabledBoxes = ()=>{
+    for(box of boxes){
+        box.disabled = true
+       }
+}
+const enableboxes = ()=>{
+    for(box of boxes){
+        box.disabled = false;
+        box.innerText = ""
+    }
+}
+const showWinner = (winner_type)=>{
+       winner.innerText =`CONGARTULATION !! Winner is ${winner_type}`
+       display_winner.style.display = "inline"
+       disabledBoxes()
+    }
+    const Hide_display_winner = ()=>{
+        display_winner.style.display = "none"
+    }
+const reset_game_or_new_game = ()=>{
+    turn_O = true;
+     enableboxes();
+     Hide_display_winner()
+}
 resetBtn.addEventListener("click",()=>{
-    
+    reset_game_or_new_game()
+})
+
+// const newGame = ()=>{
+//     turn_O =true
+//     enableboxes()
+//     Hide_display_winner()
+// }
+newGamebtn.addEventListener("click",()=>{
+  reset_game_or_new_game()
 })
